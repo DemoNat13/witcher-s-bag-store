7c7a103d4b02bcebd09aded703b54c3e120d33bc
Natalia Demakova <demakovanatalja@yandex.ru>
2024-02-21 13:33:22 +0400

    router settings

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@
         >
           <span>СУМКА</span>
           <img
-            src="assets/logo.png"
+            src="/src/assets/logo.png"
             width="70"
             height="70"
             alt="Логотип"
